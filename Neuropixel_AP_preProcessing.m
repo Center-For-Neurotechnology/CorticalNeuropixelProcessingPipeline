a2bc383a8cf40b31b9049f63b6eef508f5b941a5
Yoav Kfir <yoavkf@gmail.com>
2021-06-18 16:20:26 -0400

    Create Neuropixel_AP_preProcessing.m

---
+++ Neuropixel_AP_preProcessing.m
@@ -0,0 +1,74 @@
+function []=Neuropixel_AP_preProcessing(name, data_file, channels, start_point, end_point)
+% input:
+%     name:         string, name of subject
+%     data_file:    path to ap.bin file
+%     channels:     a vector with all channels to include (1:384)
+%     start_point:  start point in seconds
+%     end_point:    end point in seconds
+
+% no output - this will save a new bin file and additional information in
+% a mat file
+
+% preferably use only stabe time
+% dtrend each channel
+% remove median values between channels.
+% normalize channel by auto-covariance of data without outliers.
+
+save_as_bin = true;
+
+[filepath,binName,ext] = fileparts(data_file);
+output_folder = [filepath,'\',name,'_normalized\'];
+if ~exist(output_folder,'dir')
+    mkdir(output_folder)
+end
+
+
+% read ap data:
+[data, meta] = read_AP_from_bin(data_file, start_point, end_point);
+
+% detrend data:
+chanN = 384;
+tic
+for ch = channels
+    disp(['detreanding #',num2str(ch)])
+    data_array = detrend(data(ch,:));
+    data(ch,:) = data_array;
+end
+
+% Remove median:
+med =  median(data(channels,:), 1);
+% save([output_folder, 'median.mat'], 'med')
+
+
+for ch = channels 
+    disp(['removing med #',num2str(ch)])
+    data(ch,:) = data(ch,:) - med;
+end
+
+% Normalize channels:
+
+for ch = channels 
+    disp(['normilize #',num2str(ch)])
+    outliers = isoutlier(data(ch,:),'quartiles');
+    temp = std(data(ch,~outliers));
+    norm_factor(ch) = (1 / temp)   *600;
+    data(ch,:) = data(ch,:) * norm_factor(ch);
+end
+
+
+num_of_channels = length(channels);
+
+if save_as_bin
+    output_bin = 
+fid = fopen([output_folder, binName ,'.bin'],'w');
+for t = 1:size(data,2)
+    fwrite(fid,int16(data(channels,t)),'int16');
+end
+fclose(fid)
+
+save([output_folder, binName ,'.mat'], 'meta', 'med','norm_factor','num_of_channels')
+% save([output_folder, binName ,'.mat'], 'data','meta','med','norm_factor','num_of_channels','-v7.3');
+
+disp('Done!');
+end
+
